ea5955ff1ac5577772a3d896f9e9b59d95c52f98
abdul-basith06 <basitha85@gmail.com>
2024-04-16 16:29:12 +0530

    Navbar and home design done

--- Frontend/frontend/src/components/Home.jsx
+++ Frontend/frontend/src/components/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import toast, { Toaster } from 'react-hot-toast'
 
 const Home = () => {
+  const imageUrl = '/home.jpg';
   return (
-    <div>
+    <div >
       <Toaster position='top-left' reverseOrder='false' ></Toaster>
-        <h1>This is the home page</h1>
+      <div className="h-screen flex justify-center items-center z-index: 0;">
+      <img src={imageUrl} alt="Home Page Image" className="mx-auto max-w-md max-h-md rounded-lg; z-index: 0;" />
+    </div>
     </div>
   )
 }
--- Frontend/frontend/src/components/Navbar.jsx
+++ Frontend/frontend/src/components/Navbar.jsx
@@ -22,10 +22,10 @@ const Navbar = () => {
   return (
     <div>
 
-<nav className="p-5 bg-pink-50 shadow md:flex md:items-center md:justify-between">
+<nav className="p-5 bg-pink-50 shadow md:flex md:items-center md:justify-between z-index: 10;">
     <div className="flex justify-between items-center ">
-      <span className="text-2xl font-[Poppins] cursor-pointer">
-        tailwind
+      <span className="text-3xl font-[Poppins] cursor-pointer">
+        Django + React
       </span>
 
       <span className="text-3xl cursor-pointer mx-2 md:hidden block">
@@ -33,7 +33,7 @@ const Navbar = () => {
       </span>
     </div>
 
-    <ul className={`md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-pink-50 w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 ${isOpen ? 'top-[80px] opacity-100' : ''} `}>
+    <ul className={`md:flex md:items-center  md:static absolute bg-pink-50 w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 z-index: 10; ${isOpen ? 'top-[80px] opacity-100 z-index: 10;' : ''} `}>
       <li className="mx-4 my-6 md:my-0">
         <a href="#" className="text-xl hover:text-cyan-500 duration-500">HOME</a>
       </li>
@@ -53,7 +53,7 @@ const Navbar = () => {
       <button onClick={logout} className="lgt-btn">
         Logout
       </button>
-<h2 className=""></h2>
+{/* <h2 className=""></h2> */}
     </ul>
   </nav>
     </div>
