6c916c40ef42167435323f8d6a685661417d15ee
abdul-basith06 <basitha85@gmail.com>
2024-04-17 13:31:58 +0530

    User profile edit and update

--- Frontend/frontend/src/components/Profile.jsx
+++ Frontend/frontend/src/components/Profile.jsx
@@ -3,8 +3,8 @@ import { Toaster } from 'react-hot-toast'
 import { jwtDecode } from 'jwt-decode'
 import axios from 'axios'
 import toast from 'react-hot-toast'
-import { useNavigate } from 'react-router-dom';
 import { getlocal } from '../helpers/auth';
+import EditProfileModal from './modals/EditProfileModal'
 
 const Profile = () => {
     const { user_id } = jwtDecode(getlocal());
@@ -14,13 +14,6 @@ const Profile = () => {
         occupation: '',
         profile_img: '',
     });
-    const navigate = useNavigate()
-
-  const [profile_img, setProfileImage] = useState(null);
-  const [occupation, setOccupation] = useState('')
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
 
   const [isOpen, setIsOpen] = useState(false)
 
@@ -39,15 +32,17 @@ const Profile = () => {
             }
         }
         getUser();
-    }, [user_id]);
+    }, [user_id, isOpen]);
+
+
 
   return (
     <div>
   <div className='flex items-center justify-center mt-16'>
-
+  <Toaster position='top-left' reverseOrder='false' ></Toaster>
         <div className="bg-pink-50 w-1/3 mt-10 rounded-lg">
             <div className="flex items-center justify-center pt-10 flex-col">
-            <img className='rounded-full w-20' src={user.profile_img ? `http://localhost:8000${user.profile_img}/` : `https://wallpapers.com/images/featured/cool-profile-picture-87h46gcobjl5e4xu.jpg
+            <img className='rounded-full w-28 h-28' src={user.profile_img ? `http://localhost:8000${user.profile_img}/` : `https://wallpapers.com/images/featured/cool-profile-picture-87h46gcobjl5e4xu.jpg
 `} alt="profile pic" />
 <h1 className='mt-5'>{user.username}</h1>
 <h1 className='text-gray-400 text-lg p-4'>{user.occupation ? user.occupation : 'Please enter your occupation'}</h1>
@@ -60,28 +55,9 @@ const Profile = () => {
   </div>
 
 
-     {isOpen && (
-            
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-            <div className='flex items-center justify-center mt-16'>
-
-<div className="bg-pink-50 w-1/3 mt-10 rounded-lg">
-    <div className="flex items-center justify-center pt-10 flex-col">
-    <img />
-<h1 className='mt-5'>{user.username}</h1>
-<h1 className='text-gray-400 text-lg p-4'>{user.occupation ? user.occupation : 'Please enter your occupation'}</h1>
-    </div>
-    <div className='flex justify-between  mb-6'>
-        <h2 className='text-gray-400 text-lg pl-8'>{user.email}</h2>
-        <button className='edt-btn'>Edit</button>
-    </div>
-</div>
-</div>
-            </div>
-            </div>  
-           
-           ) } 
+  {isOpen && (
+        <EditProfileModal onClose={()=> setIsOpen(false)} user={user} setUser={setUser}/>
+      )}
           
 
   </div>
---
+++ Frontend/frontend/src/components/modals/EditProfileModal.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useRef, useEffect } from "react";
+import { X } from "lucide-react";
+import { jwtDecode } from 'jwt-decode'
+import { getlocal } from "../../helpers/auth";
+import toast from 'react-hot-toast'
+
+const EditProfileModal = ({  onClose, user, setUser  }) => {
+
+  const modalRef = useRef()
+  const { user_id } = jwtDecode(getlocal());
+  const [profile_img, setProfileImage] = useState('');
+  const [occupation, setOccupation] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  const closeModal=(e)=> {
+    if(modalRef.current === e.target){
+        onClose();
+    }
+  }
+
+
+
+  const updateProfile= async (e)=> {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('username', e.target.username.value);
+    formData.append('email', e.target.email.value);
+    formData.append('occupation', e.target.occupation.value);
+    formData.append('is_active',true)
+    if (profile_img) {
+        formData.append('profile_img', profile_img);
+    }
+
+    try {
+                const response = await fetch(`http://localhost:8000/api/user-update/${user_id}/`, {
+                    method: 'PUT',
+                    body: formData
+                });
+        
+                if (!response.ok) {
+                    throw new Error('Failed to update user');
+                }
+        
+                toast.success('User details updated successfully');   
+                setUser((prevUser) => ({
+                    ...prevUser,
+                    username: e.target.username.value,
+                    email: e.target.email.value,
+                    occupation: e.target.occupation.value,
+                    profile_img: profile_img ? URL.createObjectURL(profile_img) : prevUser.profile_img
+                }));
+                
+                setProfileImage('');
+                onClose()            
+            } catch (error) {
+                console.error('Failed to update user: ', error);
+                toast.error('Failed to update user');
+            }
+
+
+  };
+
+  return (
+    <div ref={modalRef} onClick={closeModal} className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center ">
+        <div className="mt-10 flex flex-col gap-5 text-black w-2/3">
+            <button onClick={onClose} className="place-self-end"><X size={30}/></button>
+            <div className="bg-pink-100 rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4">
+                <h1>Edit Profile</h1>
+                <img className='rounded-full w-28 h-28' src={user.profile_img ? `http://localhost:8000${user.profile_img}` : 'https://wallpapers.com/images/featured/cool-profile-picture-87h46gcobjl5e4xu.jpg'} alt="profile pic" />
+                <form onSubmit={updateProfile}>
+                <input
+                            type="file"
+                            name="profile_img" 
+                            onChange={e => setProfileImage(e.target.files[0])}
+                        />
+                    <input onChange={e => setUsername(e.target.value)} className="m-2" type="text" placeholder="Username" name="username" defaultValue={user.username}/>
+                    <input onChange={e => setEmail(e.target.value)} className="m-2" type="text" placeholder="email" name="email" defaultValue={user.email}/>
+                    <input onChange={e => setOccupation(e.target.value)} className="m-2" type="text" placeholder="occupation" name="occupation" defaultValue={user.occupation ? user.occupation : 'Occupation...'}/>
+                    <button className="confirm-btn m-2 p-2 w-20" type="submit">Confirm</button>
+                </form>
+            </div>
+        </div>
+    </div>
+);
+};
+
+export default EditProfileModal;
