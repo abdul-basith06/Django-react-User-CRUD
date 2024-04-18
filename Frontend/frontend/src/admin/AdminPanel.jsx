1d2ed832036e9fff0e22d3abf353d89f16c1a98c
abdul-basith06 <basitha85@gmail.com>
2024-04-18 21:22:18 +0530

    Search feature in admin side and dark mode for admin

--- Frontend/frontend/src/admin/AdminPanel.jsx
+++ Frontend/frontend/src/admin/AdminPanel.jsx
@@ -4,12 +4,14 @@ import toast, { Toaster } from "react-hot-toast";
 import Swal from "sweetalert2";
 import AddUserModal from "../components/modals/AddUserModal";
 import EditUser from "../components/modals/EditUser";
+import { useSelector } from "react-redux";
 
 const AdminPanel = () => {
   const [userList, setUserList] = useState([]);
   const [addIsOpen, setAddIsOpen] = useState(false);
   const [editIsOpen, setEditIsOpen] = useState(false);
   const [selectedUser, setSelectedUser] = useState(null);
+  const {darkMode} = useSelector((state)=> state.darkMode)
 
   useEffect(() => {
     async function getUserList() {
@@ -52,11 +54,20 @@ const AdminPanel = () => {
     setEditIsOpen(true);
   };
 
+  async function serachUser(keyword) {
+    const request = await axios.get(`http://localhost:8000/api/class-userlist/?search=${keyword}`)
+    if (request.data.length === 0) {
+
+    }
+    setUserList(request.data)
+}
+
   return (
-    <div className="flex h-screen bg-gray-100">
+    <div className= {`${
+      darkMode ? 'dark' : ''} flex h-screen bg-gray-100 dark:bg-slate-900`}>
       <Toaster position="top-left" reverseOrder="false"></Toaster>
       {/* Sidebar */}
-      <div className="flex-shrink-0 w-64 bg-white border-r">
+      <div className="flex-shrink-0 w-64 bg-white dark:bg-black border-r">
         <div className="flex flex-col h-full py-4 px-4">
           <button
             onClick={() => setAddIsOpen(true)}
@@ -69,24 +80,33 @@ const AdminPanel = () => {
 
       {/* Main Content */}
       <div className="flex-1 p-8">
-        <h1 className="text-2xl font-semibold mb-4">User List</h1>
-        <div className="bg-white shadow-md rounded-lg overflow-hidden">
+             {/* Search Bar */}
+             <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Search users..."
+            onChange={e => serachUser(e.target.value)}
+            className="px-4 py-2 border dark:text-white dark:bg-slate-800 border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+          />
+        </div>
+        <h1 className="text-2xl font-semibold mb-4 dark:text-white">User List</h1>
+        <div className="bg-white dark:bg-neutral-900 shadow-md rounded-lg overflow-hidden">
           <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
+            <thead className="bg-gray-50 dark:bg-slate-800 ">
               <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-white uppercase tracking-wider">
                   User Id
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-white uppercase tracking-wider">
                   Username
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-white uppercase tracking-wider">
                   Email
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-white uppercase tracking-wider">
                   Occupation
                 </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-white uppercase tracking-wider">
                   Delete
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -95,7 +115,7 @@ const AdminPanel = () => {
                 {/* Add more columns as needed */}
               </tr>
             </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
+            <tbody className="bg-white dark:bg-slate-800 dark:text-white divide-y divide-gray-200">
               {userList.map((user, index) => (
                 <tr key={user.id}>
                   <td className="px-6 py-4 whitespace-nowrap">{user.id}</td>
